891a305095bb793c9b8d8922e7d962654f68d125
Santi Arroyo <santi.arroyo551@gmail.com>
2023-04-19 17:51:03 -0400

    started js variables and tied them in with the html file

---
+++ script.js
@@ -0,0 +1,9 @@
+let leftAside = document.getElementById('leftAside')
+let cityForm = document.querySelector('.cityForm')
+let searchButton = document.getElementById('searchButton')
+let randomCity = document.querySelector('.randomCity')
+
+searchButton.addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log('is this working')
+})
