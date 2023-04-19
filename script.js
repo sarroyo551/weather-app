c0f8e6e1b1371d0826d8b1f8dbf251951c60937c
Santi Arroyo <santi.arroyo551@gmail.com>
2023-04-19 18:15:23 -0400

    started the fetch

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ let cityForm = document.querySelector('.cityForm')
 let searchButton = document.getElementById('searchButton')
 let randomCity = document.querySelector('.randomCity')
 
-searchButton.addEventListener('click', function (e) {
+
+function getAPI () {
+    
+let url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c1509208f603c4b4015e2841a9d5445f'
+
+fetch(url)
+    .then(function (r) {
+        return r.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+}
+function One (e) {
     e.preventDefault()
     console.log('is this working')
-})
+}
+
+searchButton.addEventListener('click', getAPI)
